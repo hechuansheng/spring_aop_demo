82566f1d6046019c2cb41f2659b2b4b4dfb14c06
hechuansheng <707708420@qq.com>
2021-04-20 10:01:48 +0800

    添加依赖

---
+++ src/main/java/org/hechuansheng/demo/aop/MainApplication.java
@@ -0,0 +1,9 @@
+package org.hechuansheng.demo.aop;
+
+/**
+ * @author : hechuansheng
+ * @date : 2021/4/20 9:36
+ * @description :
+ */
+public class MainApplication {
+}
---
+++ src/test/java/org/hechuansheng/test/aop/AopTest.java
@@ -0,0 +1,9 @@
+package org.hechuansheng.test.aop;
+
+/**
+ * @author : hechuansheng
+ * @date : 2021/4/20 9:36
+ * @description :
+ */
+public class AopTest {
+}
